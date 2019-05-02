52a0617a6feb7dec6ab33cc0e78a32069dfa0b3e
Dan O'Sullivan <dcosull2@illinois.edu>
2019-05-02 01:00:16 +0000

    Shiny App for displaying hit distance.

---
+++ ShinyHitMap.R
@@ -0,0 +1,61 @@
+library(shiny)
+library(leaflet)
+
+topDistanceBatter <- as_tibble(read.csv("topDistanceBatter2019.csv"))
+playerNames <- pull(topDistanceBatter, player_name)
+
+
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Play it where it lies!"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input ----
+      selectInput(inputId = "Player", 
+                  label = "Player", 
+                  choices = playerNames, 
+                  selected = TRUE, 
+                  multiple = FALSE,
+                  selectize = TRUE, 
+                  width = NULL, 
+                  size = NULL)
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output ----
+      leafletOutput("hitMap")
+      
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  output$hitMap <- renderLeaflet({
+    
+    currentPlayer <- topDistanceBatter %>% 
+      filter(player_name == input$Player)
+    currentPlayer
+    
+    m <- leaflet() %>%
+      addTiles() %>%  # Add default OpenStreetMap map tiles
+      addMarkers(lng=currentPlayer$homeplate_lon, lat=currentPlayer$homeplate_lat) %>% 
+      addMarkers(lng=currentPlayer$current_lon, lat=currentPlayer$current_lat)
+    
+    m
+    
+  })
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
